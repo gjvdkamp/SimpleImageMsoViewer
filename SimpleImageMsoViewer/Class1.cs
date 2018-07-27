e6d872f122babde1a025683dda67ace6e5ed8b63
gjvdkamp <gjvdkamp@gmail.com>
2018-07-27 10:35:35 +0200

    Add project files.

---
+++ SimpleImageMsoViewer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleImageMsoViewer
+{
+    public class Class1
+    {
+    }
+}
